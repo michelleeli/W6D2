41084b399de6ce8f2e2c16feb1390cb22a52bc99
Manel-Os <oulds.manel@gmail.com>
2023-06-06 16:51:42 -0400

    Strugling with 01_sql_object

--- Build Active Record/lib/01_sql_object.rb
+++ Build Active Record/lib/01_sql_object.rb
@@ -16,23 +16,37 @@ class SQLObject
   end
 
   def self.table_name
-    # ...
+    return self.to_s.downcase + "s"
   end
 
   def self.all
-    # ...
+    table_name = self.table_name
+    data = DBConnection.execute(<<-SQL)
+    
+   SELECT * FROM #{table_name}
+  SQL
+  parse_all(data)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map!{ |result| new(result) }
   end
 
   def self.find(id)
-    # ...
+    table_name = self.table_name
+    data = DBConnection.execute(<<-SQL, id)
+      SELECT * FROM #{table_name} WHERE id = ? LIMIT 1
+    SQL
+    data.first if data
+
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr,value|
+      attr_sym = attr.to_sym
+      raise"unknown attribule '#{attr}'" unless self.class.columns.include?(attr_sym)
+      send("#{attr}=", value)
+    end
   end
 
   def attributes
